d2f31f15ae5b9b06e7ca9f9c599e6e60605fedc4
samanuesam <91001903+samanuesam@users.noreply.github.com>
2023-05-04 17:55:16 +0530

    Add files via upload

---
+++ template-1/script/script.js
@@ -0,0 +1,12 @@
+function expa(){
+    let expand=document.getElementById("buttonexpand")
+    let close=document.getElementById("buttonclose")
+    close.style.display="block"
+    expand.style.display="none"
+}
+function expac(){
+    let expand=document.getElementById("buttonexpand")
+    let close=document.getElementById("buttonclose")
+    close.style.display="none"
+    expand.style.display="block"
+}
